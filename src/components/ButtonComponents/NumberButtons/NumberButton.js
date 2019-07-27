35eaefc4ff845a08ffc1c588e5b0dc9589a76d07
Jonathan Picazo <macbethjonathan@gmail.com>
2019-07-27 18:35:06 -0400

    implement button components

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
+const NumberButton = props => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      {props.number === '0' ? (
+        <button onClick={props.clickNumberBtn} name="zero" data-type="number">
+          {props.number}
+        </button>
+      ) : (
+        <button onClick={props.clickNumberBtn} data-type="number">{props.number}</button>
+      )}
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-const OperatorButton = () => {
+const OperatorButton = ({ operator: { char, value }, clickOperatorBtn }) => {
+  // Fragment: same as <> ... </>
+  // allows nested elements without showing up on the DOM
   return (
-    <>
+    <Fragment>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      <button onClick={clickOperatorBtn} data-type="operator">{char}</button>
+    </Fragment>
   );
 };
+
+export default OperatorButton;
